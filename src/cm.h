baa692d9cef2763e45b68dee9d66353867d1030f
Agustin Lozano Blua <agustinlozanoblua@gmail.com>
2022-07-11 21:34:53 -0300

    Add some definition methods and update the main

--- src/cm.h
+++ src/cm.h
@@ -11,20 +11,80 @@ using namespace std;
 
 template<class T>
 class CacheManager {
-    // <key, <Obj, index>>
     map <string, pair<T, int>> cache_data;
     int capacity;
-    bool write_file(string, T);
+    int MRU = 1;
 
     public:
-        CacheManager(int) {};
-        ~CacheManager() {};
+        CacheManager(int);
+        //~CacheManager();
 
-        void show_cache();
         void _insert(string key, T obj);
+        void show_cache();
+        bool write_file(string, T);
         T get(string key);
 
         // Any other method...
 };
+
+template <class T>
+CacheManager <T> :: CacheManager(int size) {
+    capacity = size;
+}
+
+/**
+ * Checkear a partir de la key si en necesario
+ * escribir la 'ram' ya sea para, actualizarla
+ * y que sea consistente con la cache o porque
+ * tengo que agregar un nuevo dato en ella.
+ *
+ * Esta funcionando!
+ */
+template <class T>
+bool CacheManager <T> :: write_file(string key, T obj) {
+    return (int)cache_data.size() == capacity;
+}
+
+/**
+ * A partir de la key inserto un objeto en la
+ * cache y si es necesario escribir en la 'ram'
+ *
+ * Este metodo depende de write_file
+ */
+template <class T>
+void CacheManager <T> :: _insert(string key, T obj) {
+    // Si tengo la cache vacia
+    if (cache_data.empty()) {
+        cache_data.insert(make_pair(key, make_pair(obj, MRU)));
+
+    // Si no esta vacia
+    } else {
+        //checkeo si en necesario escribir ram
+        if (write_file(key, obj)) {
+            //Investigar como escribir un archivo
+            cout << "Cache llena necesitamos escribir ram" << endl;
+        }
+
+        //inserto el nuevo objeto en la ram
+        MRU++;
+        cache_data.insert(make_pair(key, make_pair(obj, MRU)));
+    }
+
+    // Si no esta vacia
+}
+
+/**
+ * A partir de la key obtengo un objeto de la
+ * cache o de la 'ram'.
+ */
+template <class T>
+T CacheManager <T> :: get(string key) {
+    return cache_data.at(key).first;
+}
+
+template <class T>
+void CacheManager <T> :: show_cache() {
+
+}
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -15,24 +15,64 @@ public:
         cout << "Student Object: " <<
         id << ", " << value << ", " << data << endl;
     }
+    const char * getName() {
+        return data;
+    }
+
+    int getValue() {
+        return value;
+    }
 
     ~Student() {}
 };
+const string Student :: class_name = "StudentClass";
 
 // Any object can be stored in out CM
+class Purchase {
+    int ticketID;
+    int price;
+    string product_name;
+    string note;
+
+public:
+    static const string class_name;
+
+    Purchase(int _id, int _price, string _name, string _note):
+        ticketID(_id),
+        price(_price),
+        product_name(_name),
+        note(_note) {}
+
+    void print() {
+        cout << "Purchase data of " << product_name << endl;
+        cout << "\tTicket ID: " << ticketID << endl;
+        cout << "\t- Price " << price << endl;
+        cout << "\t- Notes:" << endl;
+        cout << note << endl;
+    }
+};
+const string Purchase :: class_name = "PurchaseClass";
 
 int main() {
     int cache_size = 15;
-    CacheManager<Student> my_cache(cache_size);
 
-    /*
+    CacheManager<Student> my_cache(cache_size);
+    //CacheManager<Purchase> my_cache1(cache_size);
+
+
     my_cache._insert("0", Student(0, 22, "Student 1"));
     my_cache._insert("1", Student(1, 29, "Student 2"));
-    my_cache._insert("2", Student(2, 25, "Student 3"));
-    my_cache._insert("3", Student(3, 30, "Student 4"));
+
+    cout << "Probando la funcion get de la clase CacheManager" << endl;
+    auto objFromClass = my_cache.get("0");
+    cout << "Nombre del estudiante: " << my_cache.get("0").getName() << endl;
+
+    //my_cache._insert("2", Student(2, 25, "Student 3"));
+    //my_cache._insert("3", Student(3, 30, "Student 4"));
     // ...
     // ...
 
+    /*
     my_cache.show_cache();
 
     cout << " ---------------------- UPDATE -----------------------" << endl;
